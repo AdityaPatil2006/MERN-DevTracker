2c24a2f83933541293abc8c5536713fcc15d232f
AdityaPatil1601 <adityapatil111000>
2026-06-10 11:53:11 +0530

    Prepared for Deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,37 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
+
 import { connectDB } from "./config/db.js";
 import projectsRoutes from "./routes/projectsRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5002;
+const __dirname = path.resolve();
 
 dotenv.config();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  }),
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    }),
+  );
+}
+
 app.use(express.json());
 
 app.use("/api/projects", projectsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
