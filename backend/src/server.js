04f5ba11ce6d2fd922446fd48d18ff82b6b29894
AdityaPatil1601 <adityapatil111000>
2026-06-10 11:21:10 +0530

    first commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,8 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import { connectDB } from "./src/config/db.js";
-import projectsRoutes from "./src/routes/projectsRoutes.js";
+import { connectDB } from "./config/db.js";
+import projectsRoutes from "./routes/projectsRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5002;
